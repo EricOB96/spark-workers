00e99dd3602c8a45d095be8c86436708f8f8b6f1
Eric O'Brien <ericobrien246@gmail.com>
2024-08-09 01:11:27 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,18 +1,18 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 import requests
 import os
 import json
+
 app = Flask(__name__)
 
 def get_api_key() -> str:
-    secret = os.environ.get("COMPUTE_API_KEY")
+    secret = os.environ.get("OCI_API_KEY")
     if secret:
         return secret
     else:
-        #local testing
+        # local testing
         with open('.key') as f:
-            return f.read()
+            return f.read().strip()
       
 @app.route("/")
 def hello():
@@ -20,34 +20,37 @@ def hello():
 
 @app.route("/test")
 def test():
-    #return "Test" # testing 
-    return(get_api_key())
+    # return "Test" # testing 
+    return get_api_key()
 
-@app.route("/add",methods=['GET','POST'])
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-  if request.method=='GET':
-    return "Use post to add" # replace with form template
-  else:
-    token=get_api_key()
-    ret = addWorker(token,request.form['num'])
-    return ret
-
+    if request.method == 'GET':
+        return "Use POST to add"  # replace with form template
+    else:
+        token = get_api_key()
+        ret = addWorker(token, request.form['num'])
+        return ret
 
 def addWorker(token, num):
     with open('payload.json') as p:
-      tdata=json.load(p)
-    tdata['name']='slave'+str(num)
-    data=json.dumps(tdata)
-    url='https://www.googleapis.com/compute/v1/projects/spark-371009/zones/europe-west1-b/instances'
-    headers={"Authorization": "Bearer "+token}
-    resp=requests.post(url,headers=headers, data=data)
-    if resp.status_code==200:     
-      return "Done"
+        tdata = json.load(p)
+    
+    tdata['displayName'] = 'slave' + str(num)
+    
+    data = json.dumps(tdata)
+    url = 'https://iaas.uk-london-1.oraclecloud.com/20160918/instances'
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    resp = requests.post(url, headers=headers, data=data)
+    if resp.status_code == 200:
+        return "Done"
     else:
-      print(resp.content)
-      return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
-
-
+        print(resp.content)
+        return f"Error\n{resp.content.decode('utf-8')}\n\n\n{data}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0', port=8080)
